e556b809e77ceb1d0a41d318b5afe879030092fe
charlesrt <charlesrt.digitaldwp@gmail.com>
2016-05-06 17:06:30 +0100

    3..2..1..Launch

---
+++ README.md
@@ -0,0 +1,12 @@
+Digital DWP
+========
+
+[Check it out](http://www.digital-dwp.herokuapp.com)
+
+A prototype landing page for finding out about working with Digital DWP.
+
+---
+
+Looking for a job? [We are hiring](http://digital.dwp.gov.uk/careers/search-vacancies/)
+
+---
---
+++ app/config.js
@@ -0,0 +1,19 @@
+// Use this file to change prototype configuration.
+
+// Note: prototype config can be overridden using environment variables (eg on heroku)
+
+module.exports = {
+
+  // Service name used in header. Eg: 'Renew your passport'
+  serviceName: "Digital DWP careers",
+
+  // Default port that prototype runs on
+  port: '3000',
+
+  // Enable or disable password protection on production
+  useAuth: 'false',
+
+  // Cookie warning - update link to service's cookie page.
+  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
+
+};
