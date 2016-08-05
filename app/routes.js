3eccf765797dc1642657eb3f5e55b8b6accb1f4c
charlesrt <charlesrt.digitaldwp@gmail.com>
2016-08-05 13:57:23 +0100

    Make jobs visible across the service

--- app/routes.js
+++ app/routes.js
@@ -17,6 +17,22 @@ router.get('/', function (req, res) {
   res.render('index', {data, now});
 });
 
+router.get('/open-jobs', function (req, res) {
+  var data = req.app.locals.data;
+  var now = new Date();
+
+  for (job in data) {
+  var title = data[job].title;
+      slug = title.replace(/\s+/g, '-').toLowerCase();
+      data[job].slug = slug;
+      nd = new Date(data[job].dateClosing);
+      data[job].dateClosingString = nd;
+  }
+
+  res.render('open-jobs', {data, now});
+});
+
+
 router.get('/job/:reference/:title', function(req, res) {
   var data = req.app.locals.data;
 
